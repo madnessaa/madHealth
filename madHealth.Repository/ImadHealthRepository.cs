4e6c2d6969a0e2efc8414226259ae75753c8a964
madnessaa <adnesa@hotmail.com>
2017-04-18 11:38:47 +0200

    DBRepository project

---
+++ madHealth.Repository/ImadHealthRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madHealth.Repository
+{
+    public interface ImadHealthRepository<Entity>
+    {
+        Entity Get(int id);
+        IQueryable<Entity> Get();
+
+        void Insert(Entity entity);
+        void Update(Entity entity, int id);
+        void Delete(int id);
+
+        bool Commit();
+    }
+}
